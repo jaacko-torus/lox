be3311198b28a917c5d34381652ce611dbe00df5
jaacko-torus <jaacko.torus@gmail.com>
2021-10-15 22:39:48 -0500

    12.3 - Creating Instances
    - added `LoxInstance.java`
    - changed `LoxClass` to implement `LoxCallable` allowing the creation of new instances
    - changed example to test instance creation

--- src/jlox/LoxClass.java
+++ src/jlox/LoxClass.java
@@ -1,14 +1,27 @@
 package jlox;
 
-public class LoxClass {
+import java.util.List;
+
+public class LoxClass implements LoxCallable {
 	final String name;
 
-	LoxClass(String name) {
+	public LoxClass(String name) {
 		this.name = name;
 	}
 
 	@Override
 	public String toString() {
 		return this.name;
+	}
+
+	@Override
+	public Object call(Interpreter interpreter, List<Object> arguments) {
+		LoxInstance instance = new LoxInstance(this);
+		return instance;
+	}
+
+	@Override
+	public int arity() {
+		return 0;
 	}
 }
