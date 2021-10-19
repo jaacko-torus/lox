3c5cb121b6912c009909869419154b09a9f7073a
jaacko-torus <jaacko.torus@gmail.com>
2021-10-19 14:38:04 -0500

    13.3.3 - Invalid uses of super
    - fixed common invalid uses of `super`

--- src/jlox/Resolver.java
+++ src/jlox/Resolver.java
@@ -23,7 +23,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
 	private enum ClassType {
 		NONE,
-		CLASS
+		CLASS,
+		SUBCLASS
 	}
 
 	private ClassType currentClass = ClassType.NONE;
@@ -152,6 +153,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
 	@Override
 	public Void visitSuperExpr(Expr.Super expr) {
+		if (this.currentClass == ClassType.NONE) {
+			Lox.error(expr.keyword, "Can't use \"super\" outside of a class");
+		} else if (currentClass != ClassType.SUBCLASS) {
+			Lox.error(expr.keyword, "Can't use \"super\" in a class with no superclass.");
+		}
+
 		this.resolveLocal(expr, expr.keyword);
 		return null;
 	}
@@ -204,6 +211,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 		}
 
 		if (stmt.superclass != null) {
+			this.currentClass = ClassType.SUBCLASS;
 			this.resolve(stmt.superclass);
 		}
 
