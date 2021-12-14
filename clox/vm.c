4839f6a9d68438ece068fdcf41a220c05dbdee69
jaacko-torus <jaacko.torus@gmail.com>
2021-12-14 17:27:33 -0500

    16.1.1 - Opening the compilation pipeline

--- clox/vm.c
+++ clox/vm.c
@@ -84,8 +84,7 @@ static InterpretResult run() {
 #undef READ_CONSTANT
 }
 
-InterpretResult interpret(Chunk* chunk) {
-	vm.chunk = chunk;
-	vm.ip = vm.chunk->code;
-	return run();
+InterpretResult interpret(const char* source) {
+	compile(source);
+	return INTERPRET_OK;
 }
