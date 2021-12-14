e2b98f3d875b7015ccfd7b7290f117ef4bfcbf7e
jaacko-torus <jaacko.torus@gmail.com>
2021-12-14 14:10:47 -0500

    15.2.1 - The VM's Stack

--- clox/chunk.h
+++ clox/chunk.h
@@ -18,11 +18,8 @@ typedef struct {
 } Chunk;
 
 void initChunk(Chunk* chunk);
-
 void freeChunk(Chunk* chunk);
-
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
-
 int addConstant(Chunk* chunk, Value value);
 
 #endif
