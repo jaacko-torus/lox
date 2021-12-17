fb43bff4f34e8cd37f28bc410da018806f039b91
jaacko-torus <jaacko.torus@gmail.com>
2021-12-16 20:17:02 -0500

    17.4.3 - Unary negation

--- clox/compiler.c
+++ clox/compiler.c
@@ -12,6 +12,20 @@ typedef struct {
 	bool panicMode;
 } Parser;
 
+typedef enum {
+	PREC_NONE,
+	PREC_ASSIGNMENT,	// =
+	PREC_OR,			// or
+	PREC_AND,			// and
+	PREC_EQUALITY,		// == !=
+	PREC_COMPARISON,	// < > <= >=
+	PREC_TERM,			// + -
+	PREC_FACTOR,		// * /
+	PREC_UNARY,			// ! -
+	PREC_CALL,			// . ()
+	PREC_PRIMARY
+} Precedence;
+
 Parser parser;
 Chunk* compilingChunk;
 
@@ -100,8 +114,28 @@ static void number() {
 	emitConstant(value);
 }
 
-static void expression() {
+static void parsePrecedence(Precedence precedence) {
 
+}
+
+static void expression() {
+	parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void unary() {
+	TokenType operatorType = parser.previous.type;
+
+	// Compile the operand.
+	parsePrecedence(PREC_UNARY);
+
+	// Emit the operator instruction.
+	switch (operatorType) {
+		case TOKEN_MINUS:
+			emitByte(OP_NEGATE);
+			break;
+		default:
+			return; // Unreachable.
+	}
 }
 
 static void grouping() {
