e694c782f527d31dbeb05f28658cd7830d2b538a
jaacko-torus <jaacko.torus@gmail.com>
2021-12-26 23:21:23 -0500

    21.2.0 - Variable Declarations

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -144,6 +144,19 @@ static void declaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
+static uint8_t identifierConstant(Token* name) {
+	return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static uint8_t parseVariable(const char* errorMessage) {
+	consume(TOKEN_IDENTIFIER, errorMessage);
+	return identifierConstant(&parser.previous);
+}
+
+static void defineVariable(uint8_t global) {
+	emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
 static void binary() {
 	TokenType operatorType = parser.previous.type;
 	ParseRule* rule = getRule(operatorType);
@@ -302,6 +315,19 @@ static void expression() {
 	parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void varDeclaration() {
+	uint8_t global = parseVariable("Expect variable name.");
+
+	if (match(TOKEN_EQUAL)) {
+		expression();
+	} else {
+		emitByte(OP_NIL);
+	}
+	consume(TOKEN_SEMICOLON, "Expect \";\" after variable declaration.")
+
+	defineVariable(global);
+}
+
 static void expressionStatement() {
 	expression();
 	consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
@@ -329,8 +355,7 @@ static void synchronize() {
 			case TOKEN_PRINT:
 			case TOKEN_RETURN:
 				return;
-			default:
-				; // Do nothing.
+			default:; // Do nothing.
 		}
 
 		advance();
@@ -338,7 +363,11 @@ static void synchronize() {
 }
 
 static void declaration() {
-	statement();
+	if (match(TOKEN_VAR)) {
+		varDeclaration();
+	} else {
+		statement();
+	}
 
 	if (parser.panicMode) synchronize();
 }
