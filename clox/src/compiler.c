b1b797d1f9bf1d163e9f4d6ef9a2f8a15d8023bc
jaacko-torus <jaacko.torus@gmail.com>
2021-12-28 20:51:52 -0500

    22.2.0 - Block Statements

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -40,7 +40,19 @@ typedef struct {
 	Precedence precedence;
 } ParseRule;
 
+typedef struct {
+	Token name;
+	int depth;
+} Local;
+
+typedef struct {
+	Local locals[UINT8_COUNT];
+	int localCount;
+	int scopeDepth;
+} Compiler;
+
 Parser parser;
+Compiler* current = NULL;
 Chunk* compilingChunk;
 
 static Chunk* currentChunk() {
@@ -129,6 +141,12 @@ static void emitConstant(Value value) {
 	emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
+static void initCompiler(Compiler* compiler) {
+	compiler->localCount = 0;
+	compiler->scopeDepth = 0;
+	current = compiler;
+}
+
 static void endCompiler() {
 	emitReturn();
 #ifdef DEBUG_PRINT_CODE
@@ -136,6 +154,14 @@ static void endCompiler() {
 		disassembleChunk(currentChunk(), "code");
 	}
 #endif
+}
+
+static void beginScope() {
+	current->scopeDepth++;
+}
+
+static void endScope() {
+	current->scopeDepth--;
 }
 
 static void expression();
@@ -335,6 +361,14 @@ static void expression() {
 	parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void block() {
+	while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+		declaration();
+	}
+
+	consume(TOKEN_RIGHT_BRACE, "Expect \"}\" after block.");
+}
+
 static void varDeclaration() {
 	uint8_t global = parseVariable("Expect variable name.");
 
@@ -395,6 +429,10 @@ static void declaration() {
 static void statement() {
 	if (match(TOKEN_PRINT)) {
 		printStatement();
+	} else if (match(TOKEN_LEFT_BRACE)) {
+		beginScope();
+		block();
+		endScope();
 	} else {
 		expressionStatement();
 	}
@@ -402,6 +440,8 @@ static void statement() {
 
 bool compile(const char* source, Chunk* chunk) {
 	initScanner(source);
+	Compiler compiler;
+	initCompiler(&compiler);
 	compilingChunk = chunk;
 
 	parser.hadError = false;
